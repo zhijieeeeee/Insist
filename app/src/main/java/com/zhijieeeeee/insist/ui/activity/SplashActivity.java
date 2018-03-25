d155f494db913b726a8c6722bbbcbcbcbc050741
tzhijieeeeee <tzj@onyx-international.com>
2018-03-25 12:33:07 +0800

    添加启动页

---
+++ app/src/main/java/com/zhijieeeeee/insist/ui/activity/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.zhijieeeeee.insist.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zhijieeeeee.insist.R;
+
+/**
+ * Created by Ashin on 2018/3/25.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
