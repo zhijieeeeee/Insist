4cc243038871f40b4e8184d4e953d40442ad1529
zhijieeeeee <tang562250920@gmail.com>
2018-03-28 15:30:03 +0800

    开屏页设为全屏

--- app/src/main/java/com/zhijieeeeee/insist/ui/activity/SplashActivity.java
+++ app/src/main/java/com/zhijieeeeee/insist/ui/activity/SplashActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 import com.zhijieeeeee.insist.R;
 
@@ -17,6 +18,8 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash_activity);
         new Handler().postDelayed(new Runnable() {
             @Override
