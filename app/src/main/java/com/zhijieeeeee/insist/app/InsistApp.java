dc6fb0dabeda3894fe499d2462be661cdc9d805b
tzj <3003745327@qq.com>
2018-05-28 15:47:50 +0800

    update

--- app/src/main/java/com/zhijieeeeee/insist/app/InsistApp.java
+++ app/src/main/java/com/zhijieeeeee/insist/app/InsistApp.java
@@ -7,7 +7,6 @@ import android.support.multidex.MultiDex;
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
 import com.orhanobut.logger.PrettyFormatStrategy;
-import com.squareup.leakcanary.LeakCanary;
 import com.zhijieeeeee.insist.R;
 import com.zhijieeeeee.insist.dagger.component.AppComponent;
 import com.zhijieeeeee.insist.dagger.component.DaggerAppComponent;
@@ -28,19 +27,8 @@ public class InsistApp extends Application {
     public void onCreate() {
         super.onCreate();
         mInstance = this;
-        initLeakCanary();
         initLogger();
-//        initUmeng();
         initBmob();
-    }
-
-    private void initLeakCanary() {
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
     }
 
     private void initLogger() {
@@ -50,10 +38,6 @@ public class InsistApp extends Application {
                 tag(getString(R.string.app_name)).build()));
 //        }
     }
-
-//    private void initUmeng() {
-//        UMConfigure.init(this, UMConfigure.DEVICE_TYPE_PHONE, null);
-//    }
 
     private void initBmob() {
         Bmob.initialize(this, "3d0d362654432f24abb5515799a2d5a5");
